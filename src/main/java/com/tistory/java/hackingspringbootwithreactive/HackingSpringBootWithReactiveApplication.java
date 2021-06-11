326a64e8ad05f37cd4e2199751064cb3e67cc861
LoveAndCode <practice1356@gmail.com>
2021-06-11 20:40:51 +0900

    feat: proejct init

---
+++ src/main/java/com/tistory/java/hackingspringbootwithreactive/HackingSpringBootWithReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.tistory.java.hackingspringbootwithreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackingSpringBootWithReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackingSpringBootWithReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tistory/java/hackingspringbootwithreactive/HackingSpringBootWithReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.java.hackingspringbootwithreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackingSpringBootWithReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
